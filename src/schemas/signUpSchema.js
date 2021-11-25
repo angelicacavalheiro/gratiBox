da27e692539726732280bb8217f4130d5054b886
Nivea <nivea@respondeai.com.br>
2021-11-25 17:49:48 -0300

    preparando para o review

--- src/schemas/signUpSchema.js
+++
@@ -1,9 +0,0 @@
-import Joi from 'joi';
-
-const signUpSchema = Joi.object().keys({
-  name: Joi.string().min(3).max(30).required(),
-  email: Joi.string().email({ tlds: { allow: false } }),
-  password: Joi.string().min(6).max(15),
-});
-
-export default signUpSchema;
